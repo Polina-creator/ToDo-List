15df4f1d875dc1d6617077d263c491e19b1f1cf8
Polina Podgorbunskikh <podgorbunskikh.p@gmail.com>
2021-03-31 11:01:38 +0300

    New task creates in one rendering

--- src/Components/NewTask.js
+++ src/Components/NewTask.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Input from '@material-ui/core/Input';
  
-export default function NewTask ({newTaskText, changeNewTaskText, addTaskInList}){
+export default function NewTask (){
 
-    const [newTaskText, setNewTaskText] = useState("");
+    const [newTaskText, setNewTaskText] = useState('');
     
-    
+    const creating = (e) => {
+        if (e.key==='Enter' && e.target.value!==''){
+            console.log(e.target.value);
+        }
+        
+        //console.log(e.key);
+    }
+
     // must handle task creation by Enter & input validation
-
+    
     return (
         <Input
             name = "title"
             placeholder = "New task"
-            value = {newTaskText || ''}
-            onChange = {(e) => changeNewTaskText(e)}
-            onKeyDown = {(e) => addTaskInList(e)}
+            onKeyDown = {(e) => creating(e)}
         />
     );
 }
