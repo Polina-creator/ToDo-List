14375dae82dc0feb781509fe1b2535004ecce1e9
Polina Podgorbunskikh <podgorbunskikh.p@gmail.com>
2021-04-01 12:30:06 +0300

    Separate filter function from order function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NewTask from "./Components/NewTask";
-import DateSortButtons from './Components/DateSortButtons';
-import FilterButtons from './Components/FilterButtons';
-import TasksList from './Components/TasksList';
-import {Grid, Box} from "@material-ui/core";
+import DateSortButtons from "./Components/DateSortButtons";
+import FilterButtons from "./Components/FilterButtons";
+import TasksList from "./Components/TasksList";
+import { Grid, Box } from "@material-ui/core";
 //import Task from './Components/Task';
-
 
 export default function App() {
   const [allTasks, setAllTasks] = useState([]);
   const [taskId, setTaskId] = useState(0);
-  const [filter, setFilter] = useState('All')
-  const [filteredTasks, setFilteredTasks] = useState([])
-  
+  const [filter, setFilter] = useState("All");
+  const [order, setOrder] = useState("Down");
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
   //const filteredTasks = useMemo();
-  useEffect(()=>{
+  useEffect(() => {
     handleFilter();
-  }, [allTasks, filter])
+  }, [allTasks, filter]);
+
+  useEffect(() => {
+    handleOrder();
+  }, [order])
 
   const handleFilter = () => {
-    let newArray ;
+    let newArray;
     switch (filter) {
-      case 'Up':
-        newArray = allTasks.sort((task1, task2) => task2.id - task1.id);
-        break;
-      case 'Down':
-        newArray = allTasks.sort((task1, task2) => task1.id - task2.id);
-        break;
       case "All":
         newArray = allTasks;
         break;
@@ -38,29 +36,46 @@ export default function App() {
         break;
       default:
         break;
-    } 
-    console.log(newArray);
+    }
+    //console.log(newArray);
     setFilteredTasks([...newArray]);
-  }
+  };
 
-  
+  const handleOrder = () => {
+    let orderArr;
+    switch (order) {
+      case "Up":
+        orderArr = filteredTasks.sort((task1, task2) => task2.id - task1.id);
+        break;
+      case "Down":
+        orderArr = filteredTasks.sort((task1, task2) => task1.id - task2.id);
+        break;
+      default:
+        break;
+    }
+    console.log (orderArr);
+    setFilteredTasks([...orderArr]);
+  };
 
   const addTaskInList = (newTaskText) => {
-    setAllTasks([...allTasks, {id: taskId, text: newTaskText, completed: false, date: new Date()}]);
+    setAllTasks([
+      ...allTasks,
+      { id: taskId, text: newTaskText, completed: false, date: new Date() },
+    ]);
     setTaskId(taskId + 1);
     //handleFilter();
-  }
+  };
 
   const changeCheckTask = (task) => {
-    task.completed =!  task.completed;
-    setAllTasks([...allTasks])
-  }
+    task.completed = !task.completed;
+    setAllTasks([...allTasks]);
+  };
 
   const removeTask = (removeId, e) => {
-    setAllTasks(allTasks.filter(task => task.id !== removeId));
-  }
-    
-  return(
+    setAllTasks(allTasks.filter((task) => task.id !== removeId));
+  };
+
+  return (
     <Grid>
       <Grid container justify="center">
         <Box fontSize="h3.fontSize" m={1}>
@@ -68,17 +83,17 @@ export default function App() {
         </Box>
       </Grid>
       <Grid container direction="row" justify="center" alignItems="center">
-        <NewTask addTaskInList={addTaskInList}/>
+        <NewTask addTaskInList={addTaskInList} />
       </Grid>
       <Grid container direction="row" justify="center" alignItems="center">
-      <FilterButtons setFilter={setFilter} />
-      <DateSortButtons setFilter={setFilter} />        
+        <FilterButtons setFilter={setFilter} />
+        <DateSortButtons setOrder={setOrder} />
       </Grid>
-      <TasksList 
+      <TasksList
         //handleFilter={handleFilter()}
-        filteredTasks = {filteredTasks}
-        changeCheckTask = {changeCheckTask}
-        removeTask = {removeTask}
+        filteredTasks={filteredTasks}
+        changeCheckTask={changeCheckTask}
+        removeTask={removeTask}
       />
     </Grid>
   );
--- src/Components/DateSortButtons.js
+++ src/Components/DateSortButtons.js
@@ -4,16 +4,16 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import Box from '@material-ui/core/Box';
 
-export default function DateSortButtons({setFilter}){
+export default function DateSortButtons({setOrder}){
     return( 
         <>
             <Box fontSize="h6.fontSize" m={1}>
                 Sort by Date
             </Box>
-            <IconButton id = 'Up' onClick={({currentTarget}) => setFilter(currentTarget.id)}>
+            <IconButton id = 'Up' onClick={({currentTarget}) => setOrder(currentTarget.id)}>
                 <ArrowUpwardIcon color="secondary" fontSize="large" />
             </IconButton>
-            <IconButton id = 'Down' onClick={({currentTarget}) => setFilter(currentTarget.id)}>
+            <IconButton id = 'Down' onClick={({currentTarget}) => setOrder(currentTarget.id)}>
                 <ArrowDownwardIcon color="secondary" fontSize="large" />
             </IconButton>
         </>
